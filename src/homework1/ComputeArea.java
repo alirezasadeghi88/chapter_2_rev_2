c7db014604b9aa2dd80a31a8540042d0907c2691
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-04 03:23:52 -0800

    add computarea

---
+++ src/homework1/ComputeArea.java
@@ -0,0 +1,14 @@
+package homework1;
+
+public class ComputeArea {
+    public static void main(String[] args) {
+        double radius;
+        double area;
+
+        radius = 20;
+
+        area = radius * radius * 3.14;
+
+        System.out.println(area);
+    }
+}
